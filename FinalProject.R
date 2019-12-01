a1230f40573f25d37111158ae411cb5ee81baf71
Devin Dikec <dad165@pitt.edu>
2019-12-01 17:37:35 -0500

    Version 2-Very few changes to code. Graph made to look more appealing Title, labels, and colors added for viewer

--- FinalProject.R
+++ FinalProject.R
@@ -3,7 +3,8 @@ malariadata$category<-factor(malariadata$category, levels=c("control","wild type
 meanshift<-tapply(malariadata$sporozoites,malariadata$category,mean)
 sdshift<-tapply(malariadata$sporozoites,malariadata$category,sd)
 number<-tapply(malariadata$sporozoites,malariadata$category,length)
-stripchart(sporozoites ~ category, data = malariadata, method = "jitter", vertical = TRUE)
+stripchart(main = "Sporozoite level in mosquitos",ylab = "Sporoziote level", xlab= "Mosquito type", sporozoites ~ category, data = malariadata,
+           method = "jitter",col=c("orange","red","purple"), pch=16, vertical = TRUE)
 
 
 
