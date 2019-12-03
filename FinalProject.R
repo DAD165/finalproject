953fb45899eaca5c81701c1a1ec008bf2862dc95
Devin Dikec <dad165@pitt.edu>
2019-12-02 22:53:12 -0500

    Version 3-Added text file where output will be printed Added strip chart saved to PDF file Removed some unecessary lines of code

--- FinalProject.R
+++ FinalProject.R
@@ -3,14 +3,18 @@ malariadata$category<-factor(malariadata$category, levels=c("control","wild type
 meanshift<-tapply(malariadata$sporozoites,malariadata$category,mean)
 sdshift<-tapply(malariadata$sporozoites,malariadata$category,sd)
 number<-tapply(malariadata$sporozoites,malariadata$category,length)
-stripchart(main = "Sporozoite level in mosquitos",ylab = "Sporoziote level", xlab= "Mosquito type", sporozoites ~ category, data = malariadata,
-           method = "jitter",col=c("orange","red","purple"), pch=16, vertical = TRUE)
-
-
 
 malariaanova<-lm(sporozoites ~ category,data= malariadata)
+testResult <- anova(malariaanova)
 
-anova(malariaanova)
+#Save results of ANOVA into a text file
+cat("Comparison of mean values between mosquito groups", file = "results.txt")
+cat("\n",file = "results.txt", append = TRUE)
+capture.output(testResult, file = "results.txt", append = TRUE)
+cat("\nThe null hypothesis can be rejected. There is a significant difference between the mean values of the three groups",file = "results.txt", append = TRUE)
+#Create/Save Strip Chart
+pdf("FinalProjectDataPlot.pdf")
+stripchart(main = "Sporozoite Level in Mosquitos",ylab = "Log(Sporozoite count)", xlab= "Mosquito type", sporozoites ~ category, data = malariadata,
+           method = "jitter",col=c("orange","red","purple"), pch=16, vertical = TRUE)
+dev.off()
 
-malariaAnovaSummary <- summary(anova(malariaanova))
-malariaAnovaSummary
